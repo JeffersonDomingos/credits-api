3785ec41a680c95cd0cc732b4f80d039a6e0e634
JeffersonDomingos <dominguesmorais@hotmail.com>
2025-12-15 19:27:21 -0300

    chore: initial configs to start project

---
+++ src/main/java/com/jeffersonmorais/creditsapi/CreditsApiApplication.java
@@ -0,0 +1,13 @@
+package com.jeffersonmorais.creditsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeffersonmorais/creditsapi/CreditsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffersonmorais.creditsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
