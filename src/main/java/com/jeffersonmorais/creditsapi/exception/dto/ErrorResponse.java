f2682ec62e9147cc4166aa46f08cd55e12a0c715
JeffersonDomingos <dominguesmorais@hotmail.com>
2025-12-16 08:47:00 -0300

    feature: create ErrorResponse dto

--- src/main/java/com/jeffersonmorais/creditsapi/exception/dto/ErrorResponse.java
+++ src/main/java/com/jeffersonmorais/creditsapi/exception/dto/ErrorResponse.java
@@ -1,16 +1,18 @@
-package com.jeffersonmorais.creditsapi.exception;
+package com.jeffersonmorais.creditsapi.exception.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Builder
+@Getter
 public class ErrorResponse {
 
-    private LocalDateTime timestamp;
-    private int status;
-    private String error;
-    private String message;
-    private String path;
+    private final LocalDateTime timestamp;
+    private final int status;
+    private final String error;
+    private final String message;
+    private final String path;
 
 }
