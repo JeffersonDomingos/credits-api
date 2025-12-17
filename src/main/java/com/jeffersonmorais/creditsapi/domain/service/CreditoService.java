ffbc2a05a64864471ba12a896498b2c459c1bf38
JeffersonDomingos <dominguesmorais@hotmail.com>
2025-12-17 00:06:05 -0300

    feature: config kafka in the project

--- src/main/java/com/jeffersonmorais/creditsapi/domain/service/CreditoService.java
+++ src/main/java/com/jeffersonmorais/creditsapi/domain/service/CreditoService.java
@@ -1,12 +1,15 @@
 package com.jeffersonmorais.creditsapi.domain.service;
 
+import com.jeffersonmorais.creditsapi.domain.CreditConsultedEvent;
 import com.jeffersonmorais.creditsapi.domain.entity.Credito;
 import com.jeffersonmorais.creditsapi.domain.repository.CreditoRepository;
 import com.jeffersonmorais.creditsapi.exception.CreditNotFoundException;
 import com.jeffersonmorais.creditsapi.exception.NfesNotFoundException;
+import com.jeffersonmorais.creditsapi.infrastructure.CreditEventProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -15,6 +18,9 @@ public class CreditoService {
     @Autowired
     private CreditoRepository creditoRepository;
 
+    @Autowired
+    private CreditEventProducer creditEventProducer;
+
     public List<Credito> findByNumeroNfse(String numeroNfse) {
         List<Credito> nfes = creditoRepository.findByNumeroNfse(numeroNfse);
 
@@ -22,12 +28,20 @@ public class CreditoService {
             throw new NfesNotFoundException(numeroNfse);
         }
 
+        creditEventProducer.publish(new CreditConsultedEvent("NFSE", numeroNfse, LocalDateTime.now()));
+
         return nfes;
 
     }
 
     public Credito findByNumeroCredito(String numeroCredito) {
-        return creditoRepository.findByNumeroCredito(numeroCredito).orElseThrow(() -> new CreditNotFoundException(numeroCredito));
+
+        Credito credito = creditoRepository.findByNumeroCredito(numeroCredito).orElseThrow(() -> new CreditNotFoundException(numeroCredito));
+
+        creditEventProducer.publish(new CreditConsultedEvent("Credito", numeroCredito, LocalDateTime.now()));
+
+        return credito;
+
     }
 
 }
